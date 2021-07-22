276f496215a2a863d70dc15e6f03344ebec656b5
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-07-22 14:19:26 +0100

    chore: use relative paths and add asserts

--- tests/integration/test_exec.py
+++ tests/integration/test_exec.py
@@ -1,12 +1,16 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+import os
+
 from jina import Flow, Document, DocumentArray
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_flow_from_yml(build_da):
     da = build_da()
-    with Flow.load_config('tests/integration/flow.yml') as f:
+    with Flow.load_config(os.path.join(cur_dir, 'flow.yml')) as f:
         resp = f.post(on='test', inputs=da, return_results=True)
 
     assert resp is not None
@@ -14,9 +18,10 @@ def test_flow_from_yml(build_da):
 
 def test_chunks_exists(build_da):
     da = build_da()
-    with Flow.load_config('tests/integration/flow.yml') as f:
+    with Flow.load_config(os.path.join(cur_dir, 'flow.yml')) as f:
         responses = f.post(on='segment', inputs=da, return_results=True)
 
+    assert len(responses[0].docs) == 3
     for doc in responses[0].docs:
         assert len(doc.chunks) > 0
         for chunk in doc.chunks:
--- tests/unit/test_exec.py
+++ tests/unit/test_exec.py
@@ -1,6 +1,7 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+import os
 from operator import itemgetter
 import pytest
 
@@ -8,6 +9,8 @@ from jina import Executor, Document, DocumentArray
 import cv2
 
 from yolov5_segmenter import YoloV5Segmenter
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_load():
@@ -17,8 +20,8 @@ def test_load():
 
 @pytest.mark.parametrize(
     'model_path', [
-        'tests/data/models/yolov5s.pt',
-        'tests/data/models/yolov5m.pt',
+        os.path.join(cur_dir, '../data/models/yolov5s.pt'),
+        os.path.join(cur_dir, '../data/models/yolov5m.pt'),
         'yolov5s',
         'yolov5m',
         'https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s.pt'
@@ -38,8 +41,8 @@ def test_model_name_or_path(build_da, model_path):
 
 @pytest.mark.parametrize(
     'model_path, expected_detections', [
-        ('tests/data/models/yolov5s.pt', {'bus.jpg': 5, 'zidane.jpg': 3, 'man.jpg': 3}),
-        ('tests/data/models/yolov5m.pt', {'bus.jpg': 6, 'zidane.jpg': 3, 'man.jpg': 3}),
+        (os.path.join(cur_dir, '../data/models/yolov5s.pt'), {'bus.jpg': 5, 'zidane.jpg': 3, 'man.jpg': 3}),
+        (os.path.join(cur_dir, '../data/models/yolov5m.pt'), {'bus.jpg': 6, 'zidane.jpg': 3, 'man.jpg': 3}),
     ]
 )
 def test_n_detections(build_da, model_path, expected_detections):
@@ -59,7 +62,7 @@ def test_n_detections(build_da, model_path, expected_detections):
 )
 def test_confidence_threshold(build_da, confidence_threshold, expected_detections):
     da = build_da()
-    segmenter = YoloV5Segmenter(model_name_or_path='tests/data/models/yolov5m.pt',
+    segmenter = YoloV5Segmenter(model_name_or_path=os.path.join(cur_dir, '../data/models/yolov5m.pt'),
                                 default_confidence_threshold=confidence_threshold)
     segmenter.segment(da, parameters={})
     for doc in da:
@@ -71,27 +74,24 @@ def test_traversal_paths():
     da = DocumentArray([
         Document(
             id='root',
-            blob=cv2.imread('tests/data/img/man.jpg'),
+            blob=cv2.imread(os.path.join(cur_dir, '../data/img/man.jpg')),
         ),
     ])
 
-    segmenter = YoloV5Segmenter(model_name_or_path='tests/data/models/yolov5m.pt')
+    segmenter = YoloV5Segmenter(model_name_or_path=os.path.join(cur_dir, '../data/models/yolov5m.pt'))
     segmenter.segment(da, parameters={})
 
     # detects 2 persons and 1 cell phone
     assert len(da[0].chunks) == 3
-    assert all(
-        label in ['person', 'cell phone']
-        for label in
-        map(itemgetter('label'), da[0].chunks.get_attributes('tags'))
-    )
+    assert da[0].chunks[0].tags["label"] == "person"
+    assert da[0].chunks[1].tags["label"] == "person"
+    assert da[0].chunks[2].tags["label"] == "cell phone"
 
     segmenter.segment(da, parameters={'traversal_paths': ['c']})
 
     # the first detected person spans the whole image, so segmenting the chunk produces 3 detections
-    assert len(da[0].chunks[0].chunks) == 3
-    assert all(
-        label in ['person', 'cell phone']
-        for label in
-        map(itemgetter('label'), da[0].chunks[0].chunks.get_attributes('tags'))
-    )
+    person_chunk = da[0].chunks[0]
+    assert len(person_chunk.chunks) == 3
+    assert person_chunk.chunks[0].tags["label"] == "person"
+    assert person_chunk.chunks[1].tags["label"] == "person"
+    assert person_chunk.chunks[2].tags["label"] == "cell phone"
